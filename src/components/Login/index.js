1ac1f2b7c191e3cf19a29cf0608d491765eef784
AstroKD <kdreyerinseattle@gmail.com>
2020-04-13 21:38:22 -0700

    login button centered added github and google image

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,6 +4,13 @@ import { Button } from 'react-bootstrap'
 
 import * as authActions from '../../Actions/auth'
 
+import './login.scss'
+
+//images
+import logo from '../../assets/logo.png'
+import github from '../../assets/github.png'
+import google from '../../assets/google.png'
+
 const mapStateToProps = state => {
   return {
     loggedIn: state.login.loggedIn
@@ -15,10 +22,28 @@ const mapDispatchToProps = {
 
 
 const Login = ({ loggedIn, login, children }) => {
-  console.log('Login',loggedIn)
+
   const LoginPage = (
-    <Button className="Form" onClick={login}>Log In</Button>
+    <>
+    <header>
+      <nav className="homebar">
+        <div className="homebar_logo" >  
+          <a href="/"><img className="logo" src={logo} alt="Logo" /></a>
+        </div>
+        <div>Why CLIJ?</div>
+        <div>About</div>
+      </nav>
+    </header>
+    <body>
+      <div className="login">
+        <Button onClick={login}>Log In</Button>
+        <img onClick={login} className="github" src={github} alt="github"></img>
+        <img onClick={login} className="google" src={google} alt="google"></img>
+      </div>
+    </body>
+    </>
   )
+
   if (loggedIn) {
     return children
   } else {
