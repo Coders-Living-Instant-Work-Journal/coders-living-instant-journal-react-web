210d2115916202d5d3918e17c9a1276dcf9ffd96
Tyler <tylersayvetz@gmail.com>
2020-04-15 13:49:40 -0700

    resolve conflicts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-<<<<<<< HEAD
-import { Provider, connect } from 'react-redux';
+import { connect } from 'react-redux';
 
+import MainHeader from '../MainHeader';
 import MainFooter from '../MainFooter';
 import Entries from '../Entries';
 import NewEntry from '../NewEntry';
@@ -14,25 +14,11 @@ import { changePage } from '../../Actions/pages';
 //styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-=======
-import JournalSideDrawer from '../JournalSideDrawer/JournalSideDrawer';
-import JournalBackdrop from '../Backdrop/JournalBackdrop';
-import SettingsSideDrawer from '../SettingsSideDrawer/SettingsSideDrawer';
-import SettingsBackdrop from '../Backdrop/SettingsBackdrop';
-import MainFooter from '../MainFooter'
-import Entries from '../Entries'
-import NewEntry from '../NewEntry'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from '../Login';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
->>>>>>> 8896142b6bfdc4ff397910fbc9a9616fd653c630
 
 //consts
 import { Pages } from '../../Reducers/activePageReducer';
 
-import MainHeader from '../MainHeader';
 
-<<<<<<< HEAD
 const mapStateToProps = (state) => {
   return {
     activePage: state.activePage,
@@ -51,86 +37,6 @@ function App({ activePage, changePage }) {
       </Login>
     </Container>
   );
-=======
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			journalDrawerOpen: false,
-			settingsDrawerOpen: false
-		};
-	}
-
-	///Journal Menu toggler
-	journalDrawerToggleClickHandler = () => {
-		this.setState((prevState) => {
-			return { journalDrawerOpen: !prevState.journalDrawerOpen };
-		});
-	};
-
-	journalBackdropClickHandler = () => {
-		this.setState({ journalDrawerOpen: false });
-	};
-
-	//Settings Toggler
-	settingsToggleClickHandler = () => {
-		this.setState((prevState) => {
-			return { settingsDrawerOpen: !prevState.settingsDrawerOpen };
-		});
-	};
-
-	settingsBackdropClickHandler = () => {
-		this.setState({ settingsDrawerOpen: false });
-	};
-
-	render() {
-		//Journal Menu backdrop
-		let journalBackdrop;
-		if (this.state.journalDrawerOpen) {
-			journalBackdrop = (
-				<JournalBackdrop click={this.journalBackdropClickHandler} />
-			);
-		}
-
-		//Settings backdrop
-		let settingsBackdrop;
-		if (this.state.settingsDrawerOpen) {
-			settingsBackdrop = (
-				<SettingsBackdrop settingsClick={this.settingsBackdropClickHandler} />
-			);
-		}
-
-		return (
-			<Provider store={store}>
-				<Container className="App">
-					<Login>
-					<MainHeader
-						journalDrawerClickHandler={this.journalDrawerToggleClickHandler}
-						settingsClickHandler={this.settingsToggleClickHandler}
-					/>
-					<JournalSideDrawer showJournal={this.state.journalDrawerOpen} />
-					{journalBackdrop}
-					<SettingsSideDrawer showSettings={this.state.settingsDrawerOpen} />
-					{settingsBackdrop}
-
-					<Router>
-						<Switch>
-							<Route path="/" exact>
-								<Entries />
-							</Route>
-							<Route path="/new-entry" exact>
-								<NewEntry />
-							</Route>
-						</Switch>
-						<MainFooter />
-					</Router>
-
-					</Login>
-				</Container>
-			</Provider>
-		);
-	}
->>>>>>> 8896142b6bfdc4ff397910fbc9a9616fd653c630
 }
 
 export default connect(mapStateToProps, { changePage })(App);
