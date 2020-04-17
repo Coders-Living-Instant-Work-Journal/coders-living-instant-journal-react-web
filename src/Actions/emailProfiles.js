764aca8af3cb1006b54d747822fbbae6906a925d
caitleigh <caitleigh27@gmail.com>
2020-04-17 10:53:08 -0700

    ,fixed white backgound on App for mobile view, debugged on email Profiles and put configs back to API

--- src/Actions/emailProfiles.js
+++ src/Actions/emailProfiles.js
@@ -4,51 +4,53 @@ import superagent from 'superagent'
 import cookie from 'react-cookies'
 // consts
 import { API_SERVER_URI } from '../config/config'
-const token = cookie.load('Auth-Token')
+const token = () => {
+  return cookie.load('Auth-Token')
+}
 
-export function createEmailProfile (newProfile) {
+export function createEmailProfile(newProfile) {
   return async function (dispatch) {
     const response = await superagent.post(`${API_SERVER_URI}/createEmailProfile`)
-      .set('Authorization', `bearer ${token}`)
+      .set('Authorization', `bearer ${token()}`)
       .send(newProfile)
     dispatch(CEPDispatch(response.body.email))
   }
 }
 
-function CEPDispatch (profile) {
+function CEPDispatch(profile) {
   return { type: 'CREATE_EMAIL_PROFILE', payload: profile }
 }
 
-export function updateEmailProfile (profile) {
+export function updateEmailProfile(profile) {
   return async function (dispatch) {
-    const response = await superagent.put(`${API_SERVER_URI}/updateEmailProfile`).set('Authorization', `bearer ${token}`)
+    const response = await superagent.put(`${API_SERVER_URI}/updateEmailProfile`).set('Authorization', `bearer ${token()}`)
       .send(profile)
     dispatch(UEPDispatch(response.body))
   }
 }
 
-function UEPDispatch (data) {
+function UEPDispatch(data) {
   return { type: 'UPDATE_EMAIL_PROFILE', payload: data }
 }
 
-export function getEmailProfiles () {
+export function getEmailProfiles() {
   return async function (dispatch) {
-    const response = await superagent.get(`${API_SERVER_URI}/readEmailProfiles`).set('Authorization', `bearer ${token}`)
+    const response = await superagent.get(`${API_SERVER_URI}/readEmailProfiles`).set('Authorization', `bearer ${token()}`)
     console.log(response, 'response.body')
     dispatch(GEPDispatch(response.body))
   }
 }
-function GEPDispatch (data) {
+function GEPDispatch(data) {
   return { type: 'GET_EMAIL_PROFILE', payload: data }
 }
 
-export function deleteEmailProfile (id) {
+export function deleteEmailProfile(id) {
   return async function (dispatch) {
-    const response = await superagent.delete(`${API_SERVER_URI}/deleteEmailProfile`).set('Authorization', `bearer ${token}`).send({ id: id })
+    const response = await superagent.delete(`${API_SERVER_URI}/deleteEmailProfile`).set('Authorization', `bearer ${token()}`).send({ id: id })
     dispatch(DEPDispatch(response.body.deletedProfile))
   }
 }
 
-function DEPDispatch (data) {
+function DEPDispatch(data) {
   return { type: 'DELETE_EMAIL_PROFILE', payload: data }
 }
